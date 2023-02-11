f8d9180cf8cd8f97e357a25ead31480c8801ef43
[mayank pradhan] <mayankpradhan115@gmail.com>
2023-02-11 10:38:57 +0530

    commit

---
+++ src/context/filter_context.js
@@ -0,0 +1,36 @@
+import { createContext, useContext, useEffect, useReducer } from "react";
+import {useProductContext} from "./productcontex"
+import reducer from "../reducer/filterReducer" 
+
+const FilterContext = createContext();
+
+const initialState ={
+    filter_products:[],
+    all_products: [],
+    grid_view: true,
+
+}
+export  const FilterContextProvider = ({children})=>{
+   
+   const {products} = useProductContext();
+
+   const [state, dispatch] = useReducer( reducer, initialState)
+
+   const setGridView=()=>{
+    return dispatch({type:"SET_GRID_VIEW"})
+   }
+
+   const setListView=()=>{
+    return dispatch({type:"SET_LIST_VIEW"})
+   }
+
+   useEffect(()=>{
+   dispatch({type: "LOAD_FILTER_PRODUCTS", payload: products})
+   },[products])
+
+    return <FilterContext.Provider value={{...state, setGridView, setListView}}>{children}</FilterContext.Provider>
+}
+
+export const useFilterContext = ()=>{
+    return useContext(FilterContext)
+} 
